528e4f510dfcb312a4bada906a386a4dff284c52
Vando Pinto <vandopintoo@gmail.com>
2024-04-24 09:38:14 -0300

    ajustes no script

--- src/script.js
+++ src/script.js
@@ -1,66 +1,46 @@
 $(document).ready(function () {
-
-    // $(".imagem-produto").hover(
-    //     function () {
-    //         console.log($(this)[0].currentSrc);
-    //         // $(this).attr("src", "./img/imagens-oculos/imagem1.png"); // Certifique-se de que esta é a imagem correta
-    //     },
-    //     function () {
-    //         // $(this).attr("src", "./img/imagens-oculos/front.png");
-    //     }
-    // );
-
-    // $('.icone-like').click(function () {
-    //     $(this).toggleClass('clicked');
-    // });
-
-    // Variável para armazenar os dados do banco de dados JSON
     var database;
+    var categoria;
 
-    // Função para carregar os itens da categoria correspondente
+    function adicionarItem(produto) {
+        var li = $("<li>").addClass("item");
+        var divCircle = $("<div>").addClass("circle");
+        var svgElement = $('<svg xmlns="http://www.w3.org/2000/svg" class="utilities__icons ico--wishlist icone-like" width="21" height="21" viewBox="0 0 21 21" fill="none"><path d="M15.057 0.876321C12.5247 0.830199 11.1787 2.83649 10.5171 4.10483C9.19393 1.59121 7.77948 0.761017 5.58936 0.876321C3.03423 0.991625 0.775676 3.13628 0.866931 6.38785C1.00381 12.2222 10.5399 20.5011 10.5399 20.5011C10.5399 20.5011 14.327 16.9497 16.8137 13.7212C17.8631 12.3606 20.2129 8.97066 20.2129 6.24949C20.2129 3.52831 18.2738 0.922442 15.057 0.876321Z" stroke="#000" stroke-width="0.75" stroke-miterlimit="10"/></svg>');
+        var imgLike = $("<img>").addClass("icone-like")
+            .attr("src", "data:image/svg+xml;base64," + btoa(svgElement.prop('outerHTML')))
+            .attr("alt", "Like produto");
+        var link = $("<a>").attr("href", "https://api.whatsapp.com/send?phone=556198366-5716&text=Olá,%20gostaria%20de%20mais%20informações").attr("target", "_blank");
+        var imgProduto = $("<img>").addClass("imagem-produto").attr("src", produto.imgs[0]).attr("alt", "Óculos");
+        var descricaoProduto = $("<p>").addClass("descricao-produto").text(produto.descricao);
+        var divPrecos = $("<div>").addClass("precos");
+        var precoAntigo = $("<p>").addClass("preco-antigo").text("R$ " + produto.precoAntigo);
+        var precoNovo = $("<p>").addClass("preco-novo").text("R$ " + produto.precoNovo);
+        var parcelado = $("<p>").addClass("parcelado").text("ou " + produto.parcelado[0] + "x R$ " + produto.parcelado[1]);
+
+        divCircle.append(imgLike);
+        li.append(divCircle);
+        link.append(imgProduto);
+        link.append(descricaoProduto);
+        divPrecos.append(precoAntigo);
+        divPrecos.append(precoNovo);
+        divPrecos.append(parcelado);
+        link.append(divPrecos);
+        li.append(link);
+
+        $(".itens").append(li);
+    }
+
     function carregarItensCategoria(categoria) {
-        // Limpar a lista de itens
         $(".itens").empty();
-
-        // Verificar se a categoria existe no banco de dados
         if (database.hasOwnProperty(categoria)) {
-            // Iterar sobre os produtos da categoria
             $.each(database[categoria], function (index, produto) {
-                // Criar elementos HTML para cada produto
-                var li = $("<li>").addClass("item");
-                var divCircle = $("<div>").addClass("circle");
-                var imgLike = $("<img>").addClass("icone-like").attr("src", "./img/icons/coracao.svg").attr("alt", "Like produto");
-                var link = $("<a>").attr("href", "https://api.whatsapp.com/send?phone=556198366-5716&text=Olá,%20gostaria%20de%20mais%20informações").attr("target", "_blank");
-                var imgProduto = $("<img>").addClass("imagem-produto").attr("src", produto.imgs[0]).attr("alt", "Óculos");
-                var descricaoProduto = $("<p>").addClass("descricao-produto").text(produto.descricao);
-                var divPrecos = $("<div>").addClass("precos");
-                var precoAntigo = $("<p>").addClass("preco-antigo").text("R$ " + produto.precoAntigo);
-                var precoNovo = $("<p>").addClass("preco-novo").text("R$ " + produto.precoNovo);
-                var parcelado = $("<p>").addClass("parcelado").text("ou " + produto.parcelado[0] + "x R$ " + produto.parcelado[1]);
-
-                // Adicionar elementos ao DOM
-                divCircle.append(imgLike);
-                li.append(divCircle);
-                link.append(imgProduto);
-                link.append(descricaoProduto);
-                divPrecos.append(precoAntigo);
-                divPrecos.append(precoNovo);
-                divPrecos.append(parcelado);
-                link.append(divPrecos);
-                li.append(link);
-
-                // Adicionar o item à lista de itens
-                $(".itens").append(li);
+                adicionarItem(produto);
             });
         }
     }
 
-    // Fazendo a solicitação fetch para o arquivo JSON
     $.getJSON("./src/banco-de-dados.json", function (data) {
-        // Armazenar os dados do banco de dados na variável
         database = data;
-
-        // Preencher a lista de categorias com os nomes das categorias do banco de dados
         $.each(database, function (categoria) {
             var categoriaItem = $("<li>").text(categoria).addClass('lista-categorias');
             if (categoria === "GLAMUROSAS") {
@@ -69,85 +49,54 @@ $(document).ready(function () {
             $(".categorias ul").append(categoriaItem);
         });
 
-        // Inicialmente carregar os itens da categoria "GLAMUROSAS"
         carregarItensCategoria("GLAMUROSAS");
+        categoria = "GLAMUROSAS";
 
-        // Manipular o evento de clique nos itens da lista de categorias
         $(".categorias li").click(function () {
-            // Remover a classe 'selecionado' de todas as categorias
             $(".categorias li").removeClass("selecionado");
-            // Adicionar a classe 'selecionado' à categoria selecionada
             $(this).addClass("selecionado");
-
-            // Obter o texto do item clicado (nome da categoria)
-            var categoria = $(this).text().trim();
-            // Carregar os itens da categoria correspondente
-            carregarItensCategoria(categoria);
+            var categoriaEscolhida = $(this).text().trim();
+            carregarItensCategoria(categoriaEscolhida);
+            categoria = categoriaEscolhida;
         });
     });
 
-    // Manipular o evento de digitação no campo de busca
     $(".search-bar input").on("input", function () {
-        // Obter o valor de entrada do usuário
         var query = $(this).val();
-        // Filtrar os produtos com base na consulta
-        filtrarProdutos(query);
+        filtrarProdutos(query, categoria);
     });
 
-    // Função para filtrar os produtos com base na entrada do usuário
-    function filtrarProdutos(query) {
-        // Limpar a lista de itens
+    function filtrarProdutos(query, categoriaSelecionada) {
         $(".itens").empty();
-
-        // Variável para rastrear se algum produto correspondente foi encontrado
         var encontrouCorrespondencia = false;
 
-        // Iterar sobre as categorias no banco de dados
-        $.each(database, function (categoria, produtos) {
-            // Iterar sobre os produtos dentro de cada categoria
-            $.each(produtos, function (index, produto) {
-                // Verificar se a descrição do produto contém a consulta (ignorando maiúsculas e minúsculas)
+        if (database.hasOwnProperty(categoriaSelecionada)) {
+            $.each(database[categoriaSelecionada], function (index, produto) {
                 if (produto.descricao.toLowerCase().includes(query.toLowerCase())) {
-                    // Criar elementos HTML para cada produto correspondente
-                    var li = $("<li>").addClass("item");
-                    var divCircle = $("<div>").addClass("circle");
-                    var imgLike = $("<img>").addClass("icone-like").attr("src", "./img/icons/coracao.svg").attr("alt", "Like produto");
-                    var link = $("<a>").attr("href", "https://api.whatsapp.com/send?phone=556198366-5716&text=Olá,%20gostaria%20de%20mais%20informações").attr("target", "_blank");
-                    var imgProduto = $("<img>").addClass("imagem-produto").attr("src", produto.imgs[0]).attr("alt", "Óculos");
-                    var descricaoProduto = $("<p>").addClass("descricao-produto").text(produto.descricao);
-                    var divPrecos = $("<div>").addClass("precos");
-                    var precoAntigo = $("<p>").addClass("preco-antigo").text("R$ " + produto.precoAntigo);
-                    var precoNovo = $("<p>").addClass("preco-novo").text("R$ " + produto.precoNovo);
-                    var parcelado = $("<p>").addClass("parcelado").text("ou " + produto.parcelado[0] + "x R$ " + produto.parcelado[1]);
-
-                    // Adicionar elementos ao DOM
-                    divCircle.append(imgLike);
-                    li.append(divCircle);
-                    link.append(imgProduto);
-                    link.append(descricaoProduto);
-                    divPrecos.append(precoAntigo);
-                    divPrecos.append(precoNovo);
-                    divPrecos.append(parcelado);
-                    link.append(divPrecos);
-                    li.append(link);
-
-                    // Adicionar o item à lista de itens
-                    $(".itens").append(li);
-
-                    // Indicar que uma correspondência foi encontrada
+                    adicionarItem(produto);
                     encontrouCorrespondencia = true;
                 }
             });
-        });
+        }
 
-        // Verificar se nenhuma correspondência foi encontrada
         if (!encontrouCorrespondencia) {
-            // Criar uma mensagem de aviso
             var mensagemAviso = $("<li>").addClass("item-aviso").text("Nenhum produto correspondente encontrado.");
-            // Adicionar a mensagem de aviso à lista de itens
             $(".itens").append(mensagemAviso);
         }
     }
 
+    $(".categorias li").click(function () {
+        $(".categorias li").removeClass("selecionado");
+        $(this).addClass("selecionado");
+        var categoriaEscolhida = $(this).text().trim();
+        carregarItensCategoria(categoriaEscolhida);
+        categoria = categoriaEscolhida;
+        var query = $(".search-bar input").val();
+        filtrarProdutos(query, categoriaEscolhida);
+    });
 
+    $(".search-bar input").on("input", function () {
+        var query = $(this).val();
+        filtrarProdutos(query, categoria);
+    });
 });
