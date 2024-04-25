73b72c416df19143f56ce28fe0b6e24533b72d6c
Vando Pinto <vandopintoo@gmail.com>
2024-04-25 19:58:32 -0300

    adaptacao mobile

--- src/script.js
+++ src/script.js
@@ -41,7 +41,7 @@ $(document).ready(function () {
             imgLike.addClass('marcado')
         }
 
-        var link = $("<a>").attr("href", "https://api.whatsapp.com/send?phone=556198366-5716&text=Olá,%20gostaria%20de%20mais%20informações").attr("target", "_blank");
+        var link = $("<div>").click(() => { detalhesItem(produto) })//.attr("href", "./descricao.html").attr("target", "_blank");
         var imgProduto = $("<img>").addClass("imagem-produto").attr("src", produto.imgs[0]).attr("alt", "Óculos");
         var descricaoProduto = $("<p>").addClass("descricao-produto").text(produto.descricao);
         var divPrecos = $("<div>").addClass("precos");
@@ -59,9 +59,93 @@ $(document).ready(function () {
         link.append(divPrecos);
         li.append(link);
 
+        $(".resultado-busca").css('display', 'flex')
+        $(".itens").css('display', 'flex')
         $(".itens").append(li);
         changeSVG();
     }
+
+
+
+
+
+    function detalhesItem(produto) {
+        $(".categorias li").removeClass("selecionado");
+        window.scroll(0, 0);
+        var mainContainer = $("<main>");
+        var descricaoSection = $("<section>").addClass("descricao");
+        var descricaoContainer = $("<div>").addClass("descricao-container style1");
+        var imagemContainer = $("<div>").addClass("imagem-container");
+        var imgProduto = $("<img>").attr("src", produto.imgs[0]).attr("alt", "").css("max-height", "450px");
+        var textoContainer = $("<div>").addClass("texto-container style2");
+        var tituloProduto = $("<h1>").addClass("style3").text(produto.descricao);
+        var linkConhecer = $("<a>").addClass("style4").attr("href", "#mais").text("conheça mais sobre este produto");
+        var precoAntigo = $("<p>").addClass("style5").text("de " + produto.precoAntigo);
+        var precoNovo = $("<p>").addClass("style6").text("R$ " + produto.precoNovo);
+        var parcelado = $("<p>").addClass("style7").text("ou " + produto.parcelado[0] + "x de R$ " + produto.parcelado[1] + " s/ juros");
+        var urlImagemSelecionada = window.location.href.split('/index.html')[0] + '/' + produto.imgs[0].split('./')[1];
+        var linkComprar = $("<a>").addClass("style8").attr("href",
+            `https://api.whatsapp.com/send?phone=556198366-5716&text=Olá gostaria de mais informações sobre o óculos: ${produto.descricao}, com o código: ${produto.id}, ${urlImagemSelecionada} `).attr('target', '_blank').text("COMPRAR");
+
+        // Construindo a estrutura HTML
+        imagemContainer.append(imgProduto);
+        descricaoContainer.append(imagemContainer);
+        textoContainer.append(tituloProduto);
+        textoContainer.append(linkConhecer);
+        textoContainer.append(precoAntigo);
+        textoContainer.append(precoNovo);
+        textoContainer.append(parcelado);
+        textoContainer.append(linkComprar);
+        descricaoContainer.append(textoContainer);
+        descricaoSection.append(descricaoContainer);
+
+        // Compartilhar container
+        var compartilharContainer = $("<div>").addClass("compartilhar-container style9");
+        compartilharContainer.append($("<p>").addClass("style10").text("compartilhar"));
+        compartilharContainer.append($("<a>").addClass("style11").attr("href", "").text("instagram"));
+        compartilharContainer.append($("<a>").addClass("style12").attr("href", "").text("facebook"));
+        descricaoSection.append(compartilharContainer);
+
+        mainContainer.append(descricaoSection);
+
+        // Descrição do produto
+        var descricaoProdutoContainer = $("<section>").addClass("mais style13").attr('id', 'mais');
+        var descricaoProdutoSelecionado = $("<div>").addClass("descricao-produto-selecionado style14");
+        descricaoProdutoSelecionado.append($("<h1>").addClass("style15").text("DESCRIÇÃO DO PRODUTO"));
+        var descricaoProduto = $("<div>").addClass("style16");
+        produto.descricaoProduto.forEach(function (desc) {
+            descricaoProduto.append($("<p>").text(desc));
+        });
+        descricaoProdutoSelecionado.append(descricaoProduto);
+        descricaoProdutoContainer.append(descricaoProdutoSelecionado);
+
+        // Especificações do produto
+        var especificacoesProduto = $("<div>").addClass("especificacoes-produto style17");
+        especificacoesProduto.append($("<h1>").addClass("style18").text("ESPECIFICAÇÕES DO PRODUTO"));
+        var especificacoesGrid = $("<div>").addClass("especificacoes-grid");
+        var especificacoesTipo = $("<div>").addClass("especificacoes-tipo");
+        var especificacoesInfo = $("<div>").addClass("especificacoes-info");
+        var especificacoesLabels = ["Por Gênero", "Material da Lente", "Material da Armação", "Garantia", "Proteção da Lente", "Por Estilo", "Cor da Armação", "Itens Inclusos", "Por Coleção", "Cor da Lente", "Lente", "Ponte", "Haste"];
+        Object.keys(produto.especificaProduto).forEach(function (chave, index) {
+            especificacoesTipo.append($("<p>").text(especificacoesLabels[index]));
+            especificacoesInfo.append($("<p>").text(produto.especificaProduto[chave]));
+        });
+        especificacoesGrid.append(especificacoesTipo);
+        especificacoesGrid.append(especificacoesInfo);
+        especificacoesProduto.append(especificacoesGrid);
+        descricaoProdutoContainer.append(especificacoesProduto);
+        mainContainer.append(descricaoProdutoContainer);
+
+        // Adicionando o HTML ao item da lista
+        $(".itens").empty();
+        $(".resultado-busca").css('display', 'block')
+        $(".itens").css('display', 'block')
+        $(".itens").append(mainContainer);
+    }
+
+
+
+
 
     function mudarCorFillSVG($elemento, cor) {
         $elemento.find('.icone-like').addClass('marcado');
@@ -96,7 +180,22 @@ $(document).ready(function () {
 
     }
 
-    $('.like').click(carregarItensLike)
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        // Se sim, vincular o evento de clique ao elemento com a classe .menu
+        $('.menu').click(() => {
+            $('.lista-opcoes ul').toggle();
+        });
+
+        // Se sim, vincular o evento de clique a todos os elementos dentro de .lista-opcoes ul
+        $('.lista-opcoes ul').on("click", "*", function () {
+            $('.lista-opcoes ul').hide(); // Ocultar o elemento com id "content"
+        });
+    }
+
+
+
+
+    $('.like').click(carregarItensLike);
 
     function carregarItensLike() {
         $(".categorias li").removeClass("selecionado");
